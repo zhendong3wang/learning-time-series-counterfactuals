5686ef04d490cb0a1c995cd64322126330c3639b
Zhendong Wang <crossgw94@gmail.com>
2021-10-05 18:44:11 +0200

    Update the README file

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# learning-time-series-counterfactuals
-LatentCF++: Learning Time Series Counterfactuals via Latent Space Representations
+# LatentCF++: Learning Time Series Counterfactuals via Latent Space Representations
+In our paper, we adapt the LatentCF approach to time series classification and propose an improved algorithm named LatentCF++ which introduces additional constraints in the counterfactual generation process. We conduct an extensive experiment on a total of 40 datasets from the UCR archive, comparing to current state-of-the-art methods.
+
+If you find this GitHub repo useful in your research work, please consider citing our paper:
+```
+@inproceedings{wang_learning_2021,
+	title = {Learning Time Series Counterfactuals via Latent Space Representations},
+	booktitle = {International Conference on Discovery Science},
+	author = {Wang, Zhendong and Samsten, Isak, and Mochaourab, Rami, and Papapetrou, Panagiotis},
+	year = {2021},
+}
+```
+
+## Data preparation
+
+We mainly focus on the problem of binary classification tasks with univariate time series data. After filtering, a subset of 40 datasets from the [UCR archive](https://www.cs.ucr.edu/∼eamonn/time_series_data_2018/) is selected, containing various representations from different data sources. For example,TwoLeadECG represents ECG measurements in the medical domain and Wafer exemplifies sensor data in semiconductor  manufacturing. In terms of time series length, it varies from 24 (ItalyPowerDemand) to 2709 timesteps(HandOutlines) in our chosen subset.
+
+For the evaluation in our experiment, we choose to apply a standard 80/20 split for all the datasets. Also, to compensate the imbalanced target classes in specific datasets, we apply a simple upsampling  technique, to resample with duplicates for the  minority target class of training data. The random  seed is fixed to 39 when splitting data into training/testing sets and in the upsampling approach.
+
+## Experiment Setup
+
+In the experiment, we choose to train separate CNN and long short-term memory (LSTM) classification and auto-encoder models, as main components to apply the LatentCF++ frameworks. We opt to use these simpler architectures to highlight the explainable
+power of latent space representations.
+
+Thus, we construct two instantiations for both LatentCF and LatentCF++ in our experiment: 1dCNN and LSTM. We show a detailed comparison of different components and hyperparameters for each instantiation in the following table:
+
+$$
+\begin{aligned}
+\begin{array}{ l l | l l l l } 
+\textbf{Method} & \textbf{Instantiation} & \textbf{Auto-encoder} & \textbf{Classifier} & \textbf{Optimization} & \textbf{Threshold} \\ \hline
+LatentCF++ & 1dCNN & 1dCNN-AE & 1dCNN-CLF & Adam & 0.5 \\
+& LSTM & LSTM-AE & LSTM-CLF & Adam & 0.5 \\
+& 1dCNN-C & 1dCNN-C & & Adam & 0.5 \\ \hline  
+LatentCF & 1dCNN & 1dCNN-AE & 1dCNN-CLF & Vanilla GD & No \\
+& LSTM & LSTM-AE & LSTM-CLF & Vanilla GD & No \\
+\end{array}
+\end{aligned}
+$$
+
+### Implementation Details 
+
+All deep learning models are implemented in Keras. For 1dCNN-AE, LSTM-AE, and 1dCNN-C, we set the training epochs to 50; while for classification models, the training epochs are set to 150 for both 1dCNN-CLF and LSTM-CLF. To reduce overfitting and improve the generalizability of our networks, we employ early stopping during model training. Adam optimizer is employed for all networks with learning rates ranging from 0.0001 and 0.001 for different models. The batch size is set to 32 for all of them.
+
+### Baseline Models
+
+We adopt the first baseline model from the original framework, FGD, which applies the LatentCF method with only a classifier 1dCNN-CLF to perturb samples in the original feature space directly. In addition, we apply two extra baseline methods from local and global time series tweaking approaches - random shapelet forest (RSF) and the k-NN counterfactual method (k-NN).
+
+## Running the comparison
+
+The implementation code of our experiment is at [here](./latentcf-search.py). For executing the experiment for a single dataset (e.g. TwoLeadECG), simply run
+```
+python notebooks/latentcf-search.py --dataset TwoLeadECG --pos 1 --neg 2 --output twoleadecg-outfile.csv --shallow-cnn 
+```
+
+Then for running the experiment for all the datasets from UCR, run the following code:
+```
+bash run_all_datasets.sh
+```
+
+## Results
+
+The results of quantitative analysis are available at this [csv file](./results/all-results.csv). Besides, we have two examples generated from LatentCF++ as below: 
+
+![examples](./results/result-examples.png) 
+
+Illustrated in blue are the original time series and in red the generated counterfactuals of the opposite class. By inspecting these counterfactuals, domain experts can not only gain improved understandings of the classifier decision boundary, but also can gain insight on how these predictions can be reversed.
--- run_all_datasets.sh
+++ run_all_datasets.sh
@@ -1,83 +1,85 @@
 #!/bin/bash
 # datasets with size larger than 1000
-python notebooks/latentcf-search.py --dataset ItalyPowerDemand --pos 1 --neg 2 --output italypower-outfile3.csv --shallow-cnn; 
+python notebooks/latentcf-search.py --dataset TwoLeadECG --pos 1 --neg 2 --output twoleadecg-outfile.csv --shallow-cnn 
 
-python notebooks/latentcf-search.py --dataset MoteStrain --pos 1 --neg 2 --output mote-outfile3.csv --shallow-cnn;
+python notebooks/latentcf-search.py --dataset ItalyPowerDemand --pos 1 --neg 2 --output italypower-outfile.csv --shallow-cnn; 
 
-python notebooks/latentcf-search.py --dataset Wafer --pos 1 --neg -1 --output wafer-outfile3.csv;
+python notebooks/latentcf-search.py --dataset MoteStrain --pos 1 --neg 2 --output mote-outfile.csv --shallow-cnn;
 
-python notebooks/latentcf-search.py --dataset PhalangesOutlinesCorrect --pos 1 --neg 0 --output phalanges-outfile3.csv;
+python notebooks/latentcf-search.py --dataset Wafer --pos 1 --neg -1 --output wafer-outfile.csv;
 
-python notebooks/latentcf-search.py --dataset FreezerRegularTrain --pos 1 --neg 2 --output freezerregular-outfile3.csv;
+python notebooks/latentcf-search.py --dataset PhalangesOutlinesCorrect --pos 1 --neg 0 --output phalanges-outfile.csv;
 
-python notebooks/latentcf-search.py --dataset FreezerSmallTrain --pos 1 --neg 2 --output freezesmall-outfile3.csv;
+python notebooks/latentcf-search.py --dataset FreezerRegularTrain --pos 1 --neg 2 --output freezerregular-outfile.csv;
 
-python notebooks/latentcf-search.py --dataset FordA --pos 1 --neg -1 --output forda-outfile3.csv; 
+python notebooks/latentcf-search.py --dataset FreezerSmallTrain --pos 1 --neg 2 --output freezesmall-outfile.csv;
 
-python notebooks/latentcf-search.py --dataset FordB --pos 1 --neg -1 --output fordb-outfile3.csv; 
+python notebooks/latentcf-search.py --dataset FordA --pos 1 --neg -1 --output forda-outfile.csv; 
 
-python notebooks/latentcf-search.py --dataset HandOutlines --pos 1 --neg 0 --output hand-outfile3.csv 
+python notebooks/latentcf-search.py --dataset FordB --pos 1 --neg -1 --output fordb-outfile.csv; 
+
+python notebooks/latentcf-search.py --dataset HandOutlines --pos 1 --neg 0 --output hand-outfile.csv 
 
 # datasets with size between 500 and 1000
-python notebooks/latentcf-search.py --dataset Strawberry --pos 1 --neg 2 --output strawberry-outfile3.csv; 
+python notebooks/latentcf-search.py --dataset Strawberry --pos 1 --neg 2 --output strawberry-outfile.csv; 
 
-python notebooks/latentcf-search.py --dataset SonyAIBORobotSurface2 --pos 1 --neg 2 --output sony2-outfile3.csv --shallow-cnn; 
+python notebooks/latentcf-search.py --dataset SonyAIBORobotSurface2 --pos 1 --neg 2 --output sony2-outfile.csv --shallow-cnn; 
 
-python notebooks/latentcf-search.py --dataset SemgHandGenderCh2 --pos 1 --neg 2 --output semg2-outfile3.csv; 
+python notebooks/latentcf-search.py --dataset SemgHandGenderCh2 --pos 1 --neg 2 --output semg2-outfile.csv; 
 
-python notebooks/latentcf-search.py --dataset MiddlePhalanxOutlineCorrect --pos 1 --neg 0 --output middlephalanx-outfile3.csv --shallow-cnn; 
+python notebooks/latentcf-search.py --dataset MiddlePhalanxOutlineCorrect --pos 1 --neg 0 --output middlephalanx-outfile.csv --shallow-cnn; 
 
-python notebooks/latentcf-search.py --dataset ProximalPhalanxOutlineCorrect --pos 1 --neg 0 --output proximalphalanx-outfile3.csv --shallow-cnn; 
+python notebooks/latentcf-search.py --dataset ProximalPhalanxOutlineCorrect --pos 1 --neg 0 --output proximalphalanx-outfile.csv --shallow-cnn; 
 
-python notebooks/latentcf-search.py --dataset ECGFiveDays --pos 1 --neg 2 --output ecgfive-outfile3.csv --shallow-cnn; 
+python notebooks/latentcf-search.py --dataset ECGFiveDays --pos 1 --neg 2 --output ecgfive-outfile.csv --shallow-cnn; 
 
-python notebooks/latentcf-search.py --dataset DistalPhalanxOutlineCorrect --pos 1 --neg 0 --output distalphalanx-outfile3.csv --shallow-cnn; 
+python notebooks/latentcf-search.py --dataset DistalPhalanxOutlineCorrect --pos 1 --neg 0 --output distalphalanx-outfile.csv --shallow-cnn; 
 
-python notebooks/latentcf-search.py --dataset SonyAIBORobotSurface1 --pos 1 --neg 2 --output sony1-outfile3.csv --shallow-cnn; 
+python notebooks/latentcf-search.py --dataset SonyAIBORobotSurface1 --pos 1 --neg 2 --output sony1-outfile.csv --shallow-cnn; 
 
-python notebooks/latentcf-search.py --dataset Computers --pos 1 --neg 2 --output computers-outfile3.csv --shallow-cnn
+python notebooks/latentcf-search.py --dataset Computers --pos 1 --neg 2 --output computers-outfile.csv --shallow-cnn
 
 # datasets with size lower than 500
-python notebooks/latentcf-search.py --dataset Earthquakes --pos 1 --neg 0 --output earthquakes-outfile3.csv; 
+python notebooks/latentcf-search.py --dataset Earthquakes --pos 1 --neg 0 --output earthquakes-outfile.csv; 
 
-python notebooks/latentcf-search.py --dataset GunPointAgeSpan --pos 1 --neg 2 --output GunPointAgeSpan-outfile3.csv --shallow-cnn --shallow-lstm; 
+python notebooks/latentcf-search.py --dataset GunPointAgeSpan --pos 1 --neg 2 --output GunPointAgeSpan-outfile.csv --shallow-cnn --shallow-lstm; 
 
-python notebooks/latentcf-search.py --dataset GunPointMaleVersusFemale --pos 1 --neg 2 --output GunPointMaleVersusFemale-outfile3.csv --shallow-cnn; 
+python notebooks/latentcf-search.py --dataset GunPointMaleVersusFemale --pos 1 --neg 2 --output GunPointMaleVersusFemale-outfile.csv --shallow-cnn; 
 
-python notebooks/latentcf-search.py --dataset GunPointOldVersusYoung --pos 1 --neg 2 --output GunPointOldVersusYoung-outfile3.csv --shallow-cnn; 
+python notebooks/latentcf-search.py --dataset GunPointOldVersusYoung --pos 1 --neg 2 --output GunPointOldVersusYoung-outfile.csv --shallow-cnn; 
 
-python notebooks/latentcf-search.py --dataset Chinatown --pos 1 --neg 2 --output Chinatown-outfile3.csv --shallow-cnn; 
+python notebooks/latentcf-search.py --dataset Chinatown --pos 1 --neg 2 --output Chinatown-outfile.csv --shallow-cnn; 
 
-python notebooks/latentcf-search.py --dataset PowerCons --pos 1 --neg 2 --output PowerCons-outfile3.csv --shallow-cnn; 
+python notebooks/latentcf-search.py --dataset PowerCons --pos 1 --neg 2 --output PowerCons-outfile.csv --shallow-cnn; 
 
-python notebooks/latentcf-search.py --dataset ToeSegmentation1 --pos 1 --neg 0 --output ToeSegmentation1-outfile3.csv; 
+python notebooks/latentcf-search.py --dataset ToeSegmentation1 --pos 1 --neg 0 --output ToeSegmentation1-outfile.csv; 
 
-python notebooks/latentcf-search.py --dataset WormsTwoClass --pos 1 --neg 2 --output WormsTwoClass-outfile3.csv; 
+python notebooks/latentcf-search.py --dataset WormsTwoClass --pos 1 --neg 2 --output WormsTwoClass-outfile.csv; 
 
-python notebooks/latentcf-search.py --dataset Ham --pos 1 --neg 2 --output Ham-outfile3.csv; 
+python notebooks/latentcf-search.py --dataset Ham --pos 1 --neg 2 --output Ham-outfile.csv; 
 
-python notebooks/latentcf-search.py --dataset ECG200 --pos 1 --neg -1 --output ECG200-outfile3.csv --shallow-cnn; 
+python notebooks/latentcf-search.py --dataset ECG200 --pos 1 --neg -1 --output ECG200-outfile.csv --shallow-cnn; 
 
-python notebooks/latentcf-search.py --dataset GunPoint --pos 1 --neg 2 --output GunPoint-outfile3.csv --shallow-cnn --shallow-lstm ;
+python notebooks/latentcf-search.py --dataset GunPoint --pos 1 --neg 2 --output GunPoint-outfile.csv --shallow-cnn --shallow-lstm ;
 
-python notebooks/latentcf-search.py --dataset ShapeletSim --pos 1 --neg 0 --output ShapeletSim-outfile3.csv --shallow-lstm ; 
+python notebooks/latentcf-search.py --dataset ShapeletSim --pos 1 --neg 0 --output ShapeletSim-outfile.csv --shallow-lstm ; 
 
-python notebooks/latentcf-search.py --dataset ToeSegmentation2 --pos 1 --neg 0 --output ToeSegmentation2-outfile3.csv; 
+python notebooks/latentcf-search.py --dataset ToeSegmentation2 --pos 1 --neg 0 --output ToeSegmentation2-outfile.csv; 
 
-python notebooks/latentcf-search.py --dataset HouseTwenty --pos 1 --neg 2 --output HouseTwenty-outfile3.csv; 
+python notebooks/latentcf-search.py --dataset HouseTwenty --pos 1 --neg 2 --output HouseTwenty-outfile.csv; 
 
-# python notebooks/latentcf-search.py --dataset DodgerLoopGame --pos 1 --neg 2 --output DodgerLoopGame-outfile3.csv; # filtered due to missing values
+# python notebooks/latentcf-search.py --dataset DodgerLoopGame --pos 1 --neg 2 --output DodgerLoopGame-outfile.csv; # filtered due to missing values
 
-# python notebooks/latentcf-search.py --dataset DodgerLoopWeekend --pos 1 --neg 2 --output DodgerLoopWeekend-outfile3.csv; # filtered due to missing values
+# python notebooks/latentcf-search.py --dataset DodgerLoopWeekend --pos 1 --neg 2 --output DodgerLoopWeekend-outfile.csv; # filtered due to missing values
 
-python notebooks/latentcf-search.py --dataset Herring --pos 2 --neg 1 --output Herring-outfile3.csv --shallow-cnn --shallow-lstm; 
+python notebooks/latentcf-search.py --dataset Herring --pos 2 --neg 1 --output Herring-outfile.csv --shallow-cnn --shallow-lstm; 
 
-python notebooks/latentcf-search.py --dataset Lightning2 --pos 1 --neg -1 --output Lightning2-outfile3.csv --shallow-cnn --shallow-lstm; 
+python notebooks/latentcf-search.py --dataset Lightning2 --pos 1 --neg -1 --output Lightning2-outfile.csv --shallow-cnn --shallow-lstm; 
 
-python notebooks/latentcf-search.py --dataset Wine --pos 1 --neg 2 --output Wine-outfile3.csv --shallow-cnn --shallow-lstm; 
+python notebooks/latentcf-search.py --dataset Wine --pos 1 --neg 2 --output Wine-outfile.csv --shallow-cnn --shallow-lstm; 
 
-python notebooks/latentcf-search.py --dataset Coffee --pos 1 --neg 0 --output Coffee-outfile3.csv --shallow-lstm; 
+python notebooks/latentcf-search.py --dataset Coffee --pos 1 --neg 0 --output Coffee-outfile.csv --shallow-lstm; 
 
-python notebooks/latentcf-search.py --dataset BeetleFly --pos 1 --neg 2 --output BeetleFly-outfile3.csv; 
+python notebooks/latentcf-search.py --dataset BeetleFly --pos 1 --neg 2 --output BeetleFly-outfile.csv; 
 
-python notebooks/latentcf-search.py --dataset BirdChicken --pos 1 --neg 2 --output BirdChicken-outfile3.csv; 
+python notebooks/latentcf-search.py --dataset BirdChicken --pos 1 --neg 2 --output BirdChicken-outfile.csv; 
